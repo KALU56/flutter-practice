a7c796b0aa78edbe66cad197fc44df8c15ed6380
KALU56 <kalkidanbehailu55@gmail.com>
2025-07-10 16:10:01 +0300

    weather app

---
+++ weather_app/lib/weather_model.dart
@@ -0,0 +1,20 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String description;
+
+  Weather({
+    required this.cityName,
+    required this.temperature,
+    required this.description,
+  });
+
+  // Parse from JSON
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      cityName: json['name'],
+      temperature: json['main']['temp'].toDouble(),
+      description: json['weather'][0]['description'],
+    );
+  }
+}
